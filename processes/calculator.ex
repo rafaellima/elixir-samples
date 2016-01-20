7580b016719f75ac44d50a8292f6b461029ae1fe
Rafael Lima <rafa.cbl@gmail.com>
2016-01-20 00:35:08 -0200

    Add public interfaces for calculator

--- processes/calculator.ex
+++ processes/calculator.ex
@@ -20,4 +20,16 @@ defmodule Calculator do
     end
     loop(new_value)
   end
+
+   def value(server_pid) do
+     send(server_pid, {:value, self})
+     receive do
+       {:response, value} -> value
+     end
+   end
+
+   def add(server_pid, value), do: send(server_pid, {:add, value})
+   def sub(server_pid, value), do: send(server_pid, {:sub, value})
+   def mul(server_pid, value), do: send(server_pid, {:mul, value})
+   def div(server_pid, value), do: send(server_pid, {:div, value})
 end
