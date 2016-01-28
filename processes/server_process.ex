1a880fa56ba729dc4f09245a695ee6dae602f83a
Rafael Lima <rafa.cbl@gmail.com>
2016-01-27 23:44:06 -0200

    Add cast mode to process asynchronously

--- processes/server_process.ex
+++ processes/server_process.ex
@@ -8,18 +8,27 @@ defmodule ServerProcess do
 
   defp loop(callback_module, current_state) do
     receive do
-      {request, caller} ->
+      {:call, request, caller} ->
         {response, new_state} = callback_module.handle_call(
           request,
           current_state
         )
         send(caller, {:response, response})
+      {:cast, request} ->
+        new_state = callback_module.handle_cast(
+          request,
+          current_state
+        )
         loop(callback_module, new_state)
     end
   end
 
+  def cast(server_pid, request) do
+    send(server_pid, {:cast, request, self})
+  end
+
   def call(server_pid, request) do
-    send(server_pid, {request, self})
+    send(server_pid, {:call, request, self})
     receive do
       {:response, response} ->
         response
@@ -33,7 +42,7 @@ defmodule KeyValueStore do
   end
 
   def put(pid, key, value) do
-    ServerProcess.call(pid, {:put, key, value})
+    ServerProcess.cast(pid, {:put, key, value})
   end
 
   def get(pid, key) do
@@ -48,7 +57,7 @@ defmodule KeyValueStore do
     {:ok, HashDict.put(state, key, value)}
   end
 
-  def handle_call({:get, key}, state) do
-    {HashDict.get(state, key), state}
+  def handle_cast({:put, key, value}, state) do
+    HashDict.put(state, key, value)
   end
 end
