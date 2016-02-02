96afc331fc6f3c50c472136c1a311b366efeeb53
Rafael Lima <rafa.cbl@gmail.com>
2016-02-01 23:23:38 -0200

    Remove ServerProcess in order to use GenServer

--- processes/server_process.ex
+++ processes/server_process.ex
@@ -1,63 +1,27 @@
-defmodule ServerProcess do
-  def start(callback_module) do
-    spawn(fn ->
-      initial_state = callback_module.init
-      loop(callback_module, initial_state)
-    end)
-  end
-
-  defp loop(callback_module, current_state) do
-    receive do
-      {:call, request, caller} ->
-        {response, new_state} = callback_module.handle_call(
-          request,
-          current_state
-        )
-        send(caller, {:response, response})
-      {:cast, request} ->
-        new_state = callback_module.handle_cast(
-          request,
-          current_state
-        )
-        loop(callback_module, new_state)
-    end
-  end
-
-  def cast(server_pid, request) do
-    send(server_pid, {:cast, request, self})
-  end
-
-  def call(server_pid, request) do
-    send(server_pid, {:call, request, self})
-    receive do
-      {:response, response} ->
-        response
-    end
-  end
-end
-
 defmodule KeyValueStore do
+  use GenServer
+
   def start do
-    ServerProcess.start(__MODULE__)
+    GenServer.start(KeyValueStore, nil)
   end
 
   def put(pid, key, value) do
-    ServerProcess.cast(pid, {:put, key, value})
+    GenServer.cast(pid, {:put, key, value})
   end
 
   def get(pid, key) do
-    ServerProcess.call(pid, {:get, key})
+    GenServer.call(pid, {:get, key})
   end
 
-  def init do
-    HashDict.new
-  end
-
-  def handle_call({:put, key, value}, state) do
-    {:ok, HashDict.put(state, key, value)}
+  def init(_) do
+    { :ok, HashDict.new }
   end
 
   def handle_cast({:put, key, value}, state) do
-    HashDict.put(state, key, value)
+    {:noreply, HashDict.put(state, key, value)}
+  end
+
+  def handle_call({:get, key}, _, state) do
+    {:reply, HashDict.get(state, key), state}
   end
 end
